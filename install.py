d3ec1b9ee08c9e404896528cf5b7919ea167bbbf
FishMagic <fish_magic@ftmc.me>
2020-04-10 23:22:13 +0800

    fix python script

--- install.py
+++ install.py
@@ -5,15 +5,15 @@ import os
 links_json=requests.get("https://install.ftmc.me/links.json").text
 links=json.loads(links_json)
 links=links['type']
-print("可以安装的目标：")
-for i in range(len(links)-1):
+print("Which can be installed:")
+for i in range(len(links)):
     print("\t{}) {}".format(i,links[i]["name"]))
 while(1):
-    target=input("请选择要安装的目标（输入序号）：")
-    if target in range(len(links)-1):
+    target=int(input("Choose on to install:"))
+    if target in range(len(links)):
         break
     else:
-        print("选项不存在")
+        print("Your choose is not exist.")
 link=links[target]["link"]
 os.system("wget --no-check-certificate https://install.ftmc.me/{0} -O {0}".format(link))
 os.system("python3 {}".format(link))
