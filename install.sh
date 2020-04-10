906a1ecb1c1728af68a21bb2414c429243804cba
FishMagic <fish_magic@ftmc.me>
2020-04-10 23:04:11 +0800

    fix pip command

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install python3 python3-pip -y
 wget --no-check-certificate https://install.ftmc.me/install.py -O install.py
 chmod +x install.py
-pip3 -i https://pypi.tuna.tsinghua.edu.cn/simple install requests
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
 python3 install.py
