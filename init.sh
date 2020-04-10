7f5ae073070f7f3850329be98959684d52e95842
FishMagic <fish_magic@ftmc.me>
2020-04-10 23:17:09 +0800

    edit files

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install python3 python3-pip -y
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
+wget --no-check-certificate https://install.ftmc.me/install.sh -O install.sh
+chmod +x install.sh
+python3 install.sh
--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-sudo apt update
-sudo apt install python3 python3-pip -y
 wget --no-check-certificate https://install.ftmc.me/install.py -O install.py
 chmod +x install.py
-pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
 python3 install.py
