00837b757ccaf54d95e8817fab8f6ec5215548c1
FishMagic <fish_magic@ftmc.me>
2020-04-10 23:01:17 +0800

    fix file name

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt install python3 python3-pip -y
 wget --no-check-certificate https://install.ftmc.me/install.py -O install.py
 chmod +x install.py
 pip3 -i https://pypi.tuna.tsinghua.edu.cn/simple install requests
-python3 install.python
+python3 install.py
